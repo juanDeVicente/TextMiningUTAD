c788d8d7f9678ce58e9924c5df5e0b656f2fd349
Juan de Vicente Tortosa <juan.vicente@live.u-tad.com>
2019-05-16 17:50:27 +0200

    Updated directory name textMiningUtad to text_mining_utad

--- text_mining_utad_web/text_mining_utad_web/wsgi.py
+++ text_mining_utad_web/text_mining_utad_web/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for textMiningUtadWeb project.
+WSGI config for text_mining_utad_web project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'textMiningUtadWeb.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'text_mining_utad_web.settings')
 
 application = get_wsgi_application()
