bdfab81e4e2166bbe03bcbd93abf14daed21e59f
Juan de Vicente Tortosa <juan.vicente@live.u-tad.com>
2019-05-16 17:51:49 +0200

    Added get_tweets urls

--- text_mining_utad_web/text_mining_utad_web/urls.py
+++ text_mining_utad_web/text_mining_utad_web/urls.py
@@ -1,4 +1,4 @@
-"""textMiningUtadWeb URL Configuration
+"""text_mining_utad_web URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('get_tweets.urls'))
 ]
