2136ef34a75dd90fe97c232d823b238f17de5155
Juan de Vicente Tortosa <juan.vicente@live.u-tad.com>
2019-05-22 12:34:01 +0200

    Added form to search twitter words

--- text_mining_utad_web/get_tweets/views.py
+++ text_mining_utad_web/get_tweets/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
+from .forms import search_form
 
 
 def index(request):
-	return render(request, 'index.html')
+	return render(request, 'index.html', {'search_form': search_form})
