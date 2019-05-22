d115707aaf341083dac8dcab9fe2aad7a4c8732c
ClaudiaRodriguezM <claudiarm2409@hotmail.com>
2019-05-22 13:47:18 +0200

    preliminar de los tests

---
+++ text_mining_utad_web/get_tweets/functions_tests.py
@@ -0,0 +1,24 @@
+from selenium import webdriver
+
+# FUNCTIONS USED ON THE  SELENIUM TESTS
+
+
+def got_to_web():
+    driver = webdriver.Firefox(executable_path=r"geckodriver.exe")
+    driver.get("http://127.0.0.1:8000/")
+    return driver
+
+
+def list_is_not_empty(context):
+    try:
+        context.find_element_by_id('word0')
+        return True
+    except:
+        return False
+
+
+def textboxt_empty(context):
+    try:
+        return context.find_element_by_id('username').get_property('value') == ''
+    except:
+        return False
