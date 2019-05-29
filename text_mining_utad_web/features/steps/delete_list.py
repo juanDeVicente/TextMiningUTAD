dd81adf008dcc1c03b348879ec95de64f0bd72d1
Juan de Vicente Tortosa <juan.vicente@live.u-tad.com>
2019-05-29 02:32:51 +0200

    update travis

--- text_mining_utad_web/features/steps/delete_list.py
+++ text_mining_utad_web/features/steps/delete_list.py
@@ -10,11 +10,7 @@ def step_impl(context):
     br.find_element_by_id('id_screen_name').send_keys('rayato27')
     assert br.find_element_by_name('csrfmiddlewaretoken').is_enabled()
 
-    
     br.find_element_by_id('search_button').click()
-
-    print(br.page_source)
-    br.implicitly_wait(5)
 
     assert br.find_element_by_id('word0')
 
@@ -24,6 +20,7 @@ def step_impl(context):
 
     br = context.browser
 
+    # Fill login form and submit it (valid version)
     br.find_element_by_id('delete_button').click()
 
 
