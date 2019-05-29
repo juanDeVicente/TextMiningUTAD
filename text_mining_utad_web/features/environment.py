576b11c08c61c628a6d883d87aeb09ea6837fdc8
Juan de Vicente Tortosa <juan.vicente@live.u-tad.com>
2019-05-29 02:05:22 +0200

    update travis

--- text_mining_utad_web/features/environment.py
+++ text_mining_utad_web/features/environment.py
@@ -1,22 +1,25 @@
+import os
+
 from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 
 
 def before_all(context):
-    # For debugging purposes, you can use the Firefox driver instead.
     context.browser = webdriver.Firefox()
     context.browser.implicitly_wait(1)
-    context.browser.get('http://localhost:8000')
+    context.server_url = 'http://localhost:8000'
+    context.browser.get(context.server_url)
+    context.browser.implicitly_wait(1)
 
 
 def after_all(context):
 
     # Explicitly quits the browser, otherwise it won't once tests are done
+
     context.browser.quit()
 
 
 def before_feature(context, feature):
     # Code to be executed each time a feature is going to be tested
-    if feature.name == 'Delete button':
-        br = context.browser
-        # set the ￿￿'@' of the user we want to search
-        br.find_element_by_id('search_button').click()
+    pass
